cf2b135aba1a95da9129f8de737581d30252e47c
JacobSmith2005 <90261539+JacobSmith2005@users.noreply.github.com>
2021-11-18 14:21:34 -0500

    dffsffefe
    ffefsefes

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const router = require('express').Router();
 const app = express();
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(':memory:');
 
+db.serialize(function() {
+  db.run("CREATE TABLE lorem (info TEXT)");
+
+  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+  for (var i = 0; i < 10; i++) {
+      stmt.run("Ipsum " + i);
+  }
+  stmt.finalize();
+
+  db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+      console.log(row.id + ": " + row.info);
+  });
+});
+
+db.close();
 const path = require('path');
 
 const ejs = require('ejs');
