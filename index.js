84a0f68cee930688eb68eed92c809e0982fd22de
JacobSmith2005 <90261539+JacobSmith2005@users.noreply.github.com>
2021-11-18 14:53:58 -0500

    Lmao
    Lmao

--- index.js
+++ index.js
@@ -2,23 +2,16 @@ const express = require('express');
 const router = require('express').Router();
 const app = express();
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(':memory:');
+var db = new sqlite3.Database('Bank.db');
 
-db.serialize(function() {
-  db.run("CREATE TABLE lorem (info TEXT)");
 
-  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-  for (var i = 0; i < 10; i++) {
-      stmt.run("Ipsum " + i);
-  }
-  stmt.finalize();
-
-  db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-      console.log(row.id + ": " + row.info);
-  });
+db.get("SELECT * FROM bank", function(err, row) {
+  if (err) console.log(err);
+    console.log(row);
 });
-
 db.close();
+
+/*
 const path = require('path');
 
 const ejs = require('ejs');
@@ -51,3 +44,4 @@ var server = app.listen(5000, function () {
   var PORT = 5000
   console.log('User Conncted')
 })
+*/
